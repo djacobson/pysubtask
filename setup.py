450c50960213a345cb9c71fc8075e674d12820ce
David C. Jacobson <david@jacobsonhome.com>
2018-10-21 17:42:58 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysubtask',
+	version='0.1',
+	description='Python master-subtask class pattern designed for resilient, long-running tasks like S/FTP & Dropbox transfers',
+	url='https://github.com/djacobson/pysubtask',
+	author='David Jacobson',
+	author_email='david@jacobsonhome.com',
+	license='MIT',
+	packages=['pysubtask'],
+	zip_safe=False)
